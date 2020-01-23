c977bb22f26b602ea9101a021d934f0225adb439
techlead <belabesmohammed@gmail.com>
2020-01-23 14:33:17 +0100

    je casse tout

--- ratp-api-backend/config/db.js
+++
@@ -1,8 +0,0 @@
-require('dotenv').config();
-const mongoose = require('mongoose');
-const mongoDB = process.env.DB_URL;
-mongoose.connect(mongoDB);
-mongoose.Promise = global.Promise;
-
-module.exports = mongoose;
-
